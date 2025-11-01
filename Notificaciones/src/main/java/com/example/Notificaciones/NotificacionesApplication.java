e4f24a05d3ffc41459bb18bd099c047f84ef666f
Alexxus <oleoalexis@gmail.com>
2025-11-01 16:32:19 -0600

    cambio en el env

--- Notificaciones/src/main/java/com/example/Notificaciones/NotificacionesApplication.java
+++ Notificaciones/src/main/java/com/example/Notificaciones/NotificacionesApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NotificacionesApplication {
 
+    
 	public static void main(String[] args) {
 		SpringApplication.run(NotificacionesApplication.class, args);
 	}
